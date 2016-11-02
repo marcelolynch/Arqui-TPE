d6ed6f776096c032131ff25331d954b04d0450c3
Chelivery <marcelomlynch@gmail.com>
2016-11-02 18:01:05 -0300

    Files

---
+++ x64barebones/Kernel/asm/port.asm
@@ -0,0 +1,110 @@
+
+GLOBAL sysOutLong
+GLOBAL sysInLong
+GLOBAL sysOutWord
+GLOBAL sysOutByte
+
+section .text:
+;    sysOutLong( uint16_t addr, uint32 data);
+;                      RDI          RSI
+sysOutLong:
+	push rbx
+	push rax 
+
+	mov rbx, rdi
+	and rbx, 0xFFFF ;16 bits
+
+	mov rax, rsi
+
+	mov dx, bx
+	out dx, eax
+	
+
+	pop rax
+	pop rbx
+	ret
+
+
+;    sysOutLong( uint16_t addr, uint32 data);
+;                      RDI          RSI
+sysOutWord:
+	push rbx
+	push rax 
+
+	mov rbx, rdi
+	and rbx, 0xFFFF ;16 bits
+
+	mov rax, rsi
+
+	mov dx, bx
+	out dx, ax
+	
+
+	pop rax
+	pop rbx
+	ret
+
+;    sysOutLong( uint16_t addr, uint32 data);
+;                      RDI          RSI
+sysOutByte:
+	push rbx
+	push rax 
+
+	mov rbx, rdi
+	and rbx, 0xFFFF ;16 bits
+
+	mov rax, rsi
+
+	mov dx, bx
+	out dx, al
+	
+
+	pop rax
+	pop rbx
+	ret
+
+
+
+
+;sysInLong( uint16_t addr);
+;                 RDI
+;
+sysInLong
+	push rbx
+
+	mov rbx, rdi
+	and rbx, 0xFFFF ;16 bits
+
+	mov rax, rsi
+
+	mov dx, bx
+	in eax, dx
+
+	pop rbx
+
+	ret
+
+
+;sysInLong( uint16_t addr);
+;                 RDI
+;
+sysInByte
+	push rbx
+
+	mov rbx, rdi
+	and rbx, 0xFFFF ;16 bits
+
+	mov rax, rsi
+
+	mov dx, bx
+	in al, dx
+	and rax, 0xff;
+	
+	pop rbx
+
+	ret
+
+
+
+PCI_CONFIG_ADDRESS	EQU	0x0CF8
+PCI_CONFIG_DATA		EQU	0x0CFC
---
+++ x64barebones/Kernel/include/port.h
@@ -0,0 +1,11 @@
+#ifndef __PORT_H
+#define __PORT_H
+#include <stdint.h>
+
+void sysOutLong( uint16_t addr, uint32_t data);
+void sysOutWord( uint16_t addr, uint16_t data);
+void sysOutByte( uint16_t addr, uint8_t data);
+
+uint32_t sysInLong(uint16_t addr);
+
+#endif
